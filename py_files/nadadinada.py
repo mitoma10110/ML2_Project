0a352210a82c05acd98fca9f7d65a1f9ffd792bc
mitoma10110 <133016067+mitoma10110@users.noreply.github.com>
2024-04-02 16:42:42 +0100

    tava mal importado

--- py_files/nadadinada.py
+++ py_files/nadadinada.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 cust_basket = pd.read_csv('data/customer_basket.csv')
 cust_info = pd.read_csv('data/customer_info.csv')
-prod_map = pd.read_excel('data/product_mapping.csv')
+prod_map = pd.read_excel('data/product_mapping.xlsx')
