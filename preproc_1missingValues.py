72ff22e6aef47e3c3277a4802bc9b1d6b1a00b98
C-ph3r <margaridaimsardinha@gmail.com>
2024-04-07 15:04:23 +0100

    Started preprocessing

---
+++ preproc_1missingValues.py
@@ -0,0 +1,17 @@
+'''
+Variables with missing values:
+cust_info
+4   kids_home                   - People with no children at home
+5   teens_home                  - People with no teenagers at home
+6   number_complaints           - People who have never made a complaint
+7   distinct_stores_visited     - 
+10  typical_hour                - 
+11  lifetime_spend_vegetables   - People who do not purchase vegetables
+15  lifetime_spend_fish         - People who do not purchase fish
+22  loyalty_card_number         - People who do not have cards
+
+cust_basket: no missing values
+'''
+
+def missingValues():
+    return
---
+++ preproc_function.py
@@ -0,0 +1,5 @@
+def preprocessing_custinfo(cust_info):
+    return
+
+def preprocessing_custbasket(cust_basket):
+    return
