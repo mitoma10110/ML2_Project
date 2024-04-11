cd19d5271c268f911bb663e68315d04d7a28f029
mitoma10110 <133016067+mitoma10110@users.noreply.github.com>
2024-04-11 12:17:54 +0100

    funtions for everything + cluster initial exploration and comparison

--- py_files/utils.py
+++ py_files/utils.py
@@ -15,6 +15,21 @@ def create_dispersion_list(data, k_limit=20):
         dispersion.append(kmeans.inertia_)
     return dispersion
 
+def create_agg_clusters(data, linkage='ward', distance_threshold=0, n_clusters=None):
+    agg_clust = AgglomerativeClustering(
+        linkage=linkage, distance_threshold=distance_threshold, n_clusters=n_clusters
+        ).fit(data)
+    return agg_clust
+
+def allocate_clusters(df, data_preprocessed, n_clusters, cluster_type=['KMeans', 'AgglomerativeClustering'], random_state=0, linkage='ward'):
+    if cluster_type=='KMeans':
+        model = KMeans(n_clusters=n_clusters, random_state=random_state).fit(data_preprocessed)
+        df['cluster_kmeans'] = model.predict(data_preprocessed)
+    elif cluster_type=='AgglomerativeClustering':
+        model = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage).fit(data_preprocessed)
+        df['cluster_hierarchical'] = model.fit_predict(data_preprocessed)
+
+
 # plot funtions for k analysis
 def plot_elbow_graph(dispersion):
     '''
