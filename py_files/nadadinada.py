e13caaa93bcd4507081a5cbb0487480c4154fbaf
Loris <lourencomouraomartins@gmail.com>
2024-05-07 16:29:59 +0100

    joel gosta de penis

--- py_files/nadadinada.py
+++ py_files/nadadinada.py
@@ -3,3 +3,6 @@ import pandas as pd
 cust_basket = pd.read_csv('data/customer_basket.csv')
 cust_info = pd.read_csv('data/customer_info.csv')
 prod_map = pd.read_excel('data/product_mapping.xlsx')
+
+
+cust_info.head()
