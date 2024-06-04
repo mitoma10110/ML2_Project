ae0b500b409c8ce514a86742b162dd1100ec69a7
mitoma10110 <133016067+mitoma10110@users.noreply.github.com>
2024-06-04 17:09:25 +0100

    tirei o import de ficheiro product mapping p ficar no notebook e não no py file

--- py_files/preprocessing.py
+++ py_files/preprocessing.py
@@ -3,8 +3,7 @@ import numpy as np
 from copy import deepcopy
 from sklearn.preprocessing import RobustScaler
 from sklearn.impute import KNNImputer
-product_mapping = pd.read_excel('data\product_mapping.xlsx')
-from feature_engineering import *
+from py_files.feature_engineering import *
 
 def scaling(df:pd.DataFrame) -> pd.DataFrame:
     '''
@@ -48,7 +47,7 @@ def cust_info_preproc(df:pd.DataFrame) -> pd.DataFrame:
 
     return cust_info
 
-def cust_basket_preproc(df):
+def cust_basket_preproc(df, product_maping):
     '''
     Function that applies preprocessing to the customer basket dataset
     '''
