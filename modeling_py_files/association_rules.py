72d86bb60c1827cf5620391b24d0a11301c9cb67
Loris <lourencomouraomartins@gmail.com>
2024-06-10 22:08:00 +0100

    commitquasefinal

--- modeling_py_files/association_rules.py
+++ modeling_py_files/association_rules.py
@@ -8,15 +8,14 @@ def association_rules_preproc(data_w_clusters:pd.DataFrame, encoded_purchases:pd
     '''
     Function that creates a dictionary of cluster information to apply association rules to
     Inputs:
-    
-data_w_clusters (pd.Dataframe): Dataframe indexed by customer_id that includes the associated cluster
-      We assume the cluster column is 'cluster_solution'
-encoded_purchases (pd.Dataframe): Dataframe indexed by invoice_id that describes the customers'
-purchases, encoded with transaction encoding
+    - data_w_clusters (pd.Dataframe): Dataframe indexed by customer_id that includes the associated cluster
+        We assume the cluster column is 'cluster_solution'
+    - encoded_purchases (pd.Dataframe): Dataframe indexed by invoice_id that describes the customers'
+    purchases, encoded with transaction encoding
 
     Outputs:
-    
-cluster_dfs (dict): Dict of cluster_label | Purchases filtered for that cluster'''
+    - cluster_dfs (dict): Dict of cluster_label | Purchases filtered for that cluster
+    '''
     filtered_data = data_w_clusters[data_w_clusters['customer_id'].isin(encoded_purchases['customer_id'])]
     clusters = filtered_data['cluster_solution'].unique()
 
@@ -30,7 +29,8 @@ cluster_dfs (dict): Dict of cluster_label | Purchases filtered for that cluster'
 
     for cluster in clusters:
       clustered_baskets[cluster] = encoded_purchases_with_cluster[encoded_purchases_with_cluster['cluster_solution'] == cluster]
-      clustered_baskets[cluster].drop('cluster_solution', axis=1, inplace=True)
+      clustered_baskets[cluster].drop(['cluster_solution'], axis=1, inplace=True)
+
 
     return clustered_baskets
 
@@ -52,34 +52,8 @@ def association_rules_apriori(purchases_dict:dict, index:int) -> pd.DataFrame:
                                     metric="confidence",
                                     min_threshold=0.2)
 
-    # Filter rules where 'lift' is greater than 1
+    # Filter rules where 'lsupport' > 0.1
     rules_grocery_lift_filtered = rules_grocery[rules_grocery['support'] > 0.1]
 
     print(f'Cluster {index}:')
     display(rules_grocery_lift_filtered.sort_values(by='lift', ascending=False).head(20))
-
-    return rules_grocery_lift_filtered
-
-def association_rules_eclat(purchases_dict:dict, index:int) -> pd.DataFrame:
-    '''
-    Function that applies ECLAT association rules assuming min_support=0.05
-    Inputs:
-    - purchases_dict (dict): Dictionary of purchases per cluster
-        Created by association_rules_preproc()
-    - index (int): Key to look for in the dict
-    Outputs:
-    - Prints the frequent items and rules created of that cluster
-    - Returns the dataframe of rules
-    '''
-    eclat_groceries = ECLAT(data=purchases_dict[index])
-    
-    groceries_rules_indexes, groceries_rules_supports = eclat_groceries.fit(min_support=0.05,
-                                           min_combination=2,
-                                           max_combination=2)
-    
-    rules_eclat_groceries = pd.DataFrame(list(groceries_rules_supports.values()),
-                                    index=list(groceries_rules_supports.keys()),
-                                    columns=['support'])
-
-    print(f'Cluster {index}:')
-    return rules_eclat_groceries.sort_values(by='support', ascending=False)
