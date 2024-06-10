a684c78b5138a00c0bc130118faa5186843ac79a
mitoma10110 <133016067+mitoma10110@users.noreply.github.com>
2024-06-10 18:49:52 +0100

    faltou feature selection

--- preprocessing_py_files/feature_selection.py
+++ preprocessing_py_files/feature_selection.py
@@ -4,7 +4,7 @@ def feature_selection(df: pd.DataFrame) -> pd.DataFrame:
     '''
     Function that removes unwanted variables from the dataset
     '''
-    highly_correlated_vars = ['lifetime_spend_videogames', 'lifetime_spend_meat']
+    highly_correlated_vars = ['lifetime_spend_videogames', 'lifetime_spend_fish', 'lifetime_spend_meat']
     for var in highly_correlated_vars:
         if var in df.columns:
             df.drop(var, axis=1, inplace=True)
